37d6f5082f3a1c653122ea398a185f591bf550af
Vishwas <159612031+vishwas857@users.noreply.github.com>
2025-11-23 22:20:14 +0530

    CHANGE
    Removed debug mode from app.run()

--- app.py
+++ app.py
@@ -91,4 +91,4 @@ def show_employees():
 # Railway uses PORT environment variable
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 8000))
-    app.run(debug=True, host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0")
