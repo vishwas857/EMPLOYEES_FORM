74dc2c4ccb1822e68ecc0b129e2daabc937c78ee
Vishwas <159612031+vishwas857@users.noreply.github.com>
2025-11-23 21:18:45 +0530

    CHANGE

--- app.py
+++ app.py
@@ -89,5 +89,6 @@ def show_employees():
     return render_template('employees.html', employees=employees)
 
 # Railway uses PORT environment variable
-
-app.run(debug=True, host="0.0.0.0", port=port)
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 8000))
+    app.run(debug=True, host="0.0.0.0", port=port)
